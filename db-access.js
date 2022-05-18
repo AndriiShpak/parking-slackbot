429e0068233ff2bb5d5af888d33d87fd8f017c81
Andrii Shpak <andrii.shpak@euristiq.com>
2022-05-18 16:32:22 +0300

    Added parking bot logic

--- db-access.js
+++ db-access.js
@@ -1,31 +1,104 @@
 const { Client } = require("pg");
 
-const connectionString = process.env.DATABASE_URL;
-
-async function createTables() {
+async function executeQuery(query, parameters = null) {
   const client = new Client({
-    connectionString,
-    ssl: { rejectUnauthorized: false }
-  });
-  await client.connect();
-  const now = await client.query("SELECT NOW()");
-  await client.end();
-
-  return now;
-}
-
-async function clientDemo() {
-  const client = new Client({
-    connectionString,
+    connectionString: process.env.DATABASE_URL,
     ...(process.env.IS_LOCAL ? {} : { ssl: { rejectUnauthorized: false } })
   });
+
   await client.connect();
-  const now = await client.query("SELECT NOW()");
+  const result = await client.query(query, parameters);
   await client.end();
 
-  return now;
+  return result;
+}
+
+function getDayStart() {
+  var dayStart = new Date();
+  dayStart.setUTCHours(0,0,0,0);
+  return dayStart;
+}
+
+async function setupDb() {
+  await executeQuery(`CREATE TABLE IF NOT EXISTS park_statements (
+    id SERIAL,
+    date timestamp without time zone,
+    user_id varchar(32) NULL,
+    parking_slot smallint NOT NULL
+  )`);
+
+  await executeQuery(`CREATE TABLE IF NOT EXISTS park_blames (
+    id SERIAL,
+    date timestamp without time zone,
+    car_plate varchar(32),
+    parking_slot smallint,
+    created_by varchar(32)
+  )`);
+}
+
+async function addParkingStatement(userId, parkingSlot) {
+  await executeQuery(`INSERT INTO park_statements (date, user_id, parking_slot) VALUES ($1, $2, $3)`, [
+    new Date(),
+    userId,
+    parkingSlot
+  ]);
+}
+
+async function removeParkingStatement(userId) {
+  await executeQuery(`DELETE FROM park_statements WHERE user_id = $1 AND date > $2`, [
+    userId,
+    getDayStart()
+  ]);
+}
+
+async function getIsSlotAlreadyBooked(parkingSlot, userId) {
+  const result = await executeQuery(`SELECT user_id, parking_slot FROM park_statements WHERE (parking_slot = $1 OR user_id = $2) AND date > $3`, [
+    parkingSlot,
+    userId,
+    getDayStart()
+  ]);
+
+  return result.rows.length ? {
+    userId: result.rows[0]['user_id'],
+    parkingSlot: result.rows[0]['parking_slot']
+  } : null;
+}
+
+async function getAllParked() {
+  const result = await executeQuery(`SELECT * FROM park_statements WHERE date > $1`, [
+    getDayStart()
+  ]);
+
+  return result.rows.map(user => ({
+    parkingSlot: user['parking_slot'],
+    userId: user['user_id']
+  }));
+}
+
+async function addBlameStatement(carPlate, parkingSlot, whoBlamed) {
+  await executeQuery(`INSERT INTO park_blames (date, car_plate, parking_slot, created_by) VALUES ($1, $2, $3, $4)`, [
+    new Date(),
+    carPlate,
+    parkingSlot,
+    whoBlamed
+  ]);
+}
+
+async function getAllBlames() {
+  const result = await executeQuery(`SELECT car_plate, count(*) as count FROM park_blames GROUP BY car_plate`);
+
+  return result.rows.map(user => ({
+    carPlate: user['car_plate'],
+    count: user['count']
+  }));
 }
 
 module.exports = {
-  clientDemo,
+  setupDb,
+  addParkingStatement,
+  removeParkingStatement,
+  getIsSlotAlreadyBooked,
+  getAllParked,
+  addBlameStatement,
+  getAllBlames
 }
--- index.js
+++ index.js
@@ -1,100 +1,232 @@
 require('dotenv').config();
 const { App } = require('@slack/bolt');
-const { clientDemo } = require('./db-access');
+const {
+  setupDb,
+  addParkingStatement,
+  removeParkingStatement,
+  getIsSlotAlreadyBooked,
+  getAllParked,
+  addBlameStatement,
+  getAllBlames
+ } = require('./db-access');
 
-const parkingStore = {
-  availablePlaces: [1, 2, 3, 4, 5],
-  parked: {},
-};
+const availablePlaces = process.env.AVAILABLE_SLOTS.split(',').map(place => +place);
+const parkingSchema = process.env.IMAGE_URL;
 
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
 });
 
-const UNKNOWN_ERROR = 'Unknown error occurred.'
+const UNKNOWN_ERROR = 'Сталась невідома помилка, напиши про неї адміну';
 
-function validateParkingSlot(parkSlot, commandText) {
-  if (!(typeof parkSlot !== 'number')) {
-    return `I can't understand what parking slot you are trying to use. Your input: ${commandText}`
-  }
-
-  if (!parkingStore.availablePlaces.includes(parkSlot)) {
-    return `Parking slot that you are trying to book is not in the allowed list. Your input: ${commandText}`;
-  }
-}
-
-/* Add functionality here */
-
-app.command('/park', async ({ command, ack, respond }) => {
-  // Acknowledge command request
+app.command('/pick_nest', async ({ command, ack, respond }) => {
   await ack();
 
-  await respond({
-    response_type: 'ephemeral',
-    text: 'IT WORKS!!!'
-  });
+  try {
+    let allParked = await getAllParked();
+    let resultText;
 
-  // const parkSlot = +command.text;
-  // const error = validateParkingSlot(parkSlot, command.text);
+    if (allParked.length < availablePlaces.length) {
+      const allParkedSlots = allParked.map(parked => parked.parkingSlot);
+      const freeSpaces = availablePlaces.filter(place => !allParkedSlots.includes(place));
+      resultText = `Вибирай де хочеш приземлитись: ${freeSpaces.join(', ')}`;
+    } else {
+      resultText = `Пізно! Вільних місць не залишилось`;
+    }
 
-  // if (!error && parkingStore.parked[parkSlot]) {
-  //   error = `Slot is already booked by <@${parkingStore.parked[parkSlot]}>`
-  // }
+    await app.client.chat.postEphemeral({
+      channel: command.channel_id,
+      user: command.user_id,
+      text: resultText,
+      attachments: [
+        {
+          type: "image",
+          title: {
+            type: "plain_text",
+            text: "Parking schema"
+          },
+          image_url: parkingSchema,
+          alt_text: "Parking schema"
+        }
+      ]
+    });
+  } catch (e) {
+    console.log('!!!Error', e);
 
-  // if (error) {
-  //   await respond({
-  //     response_type: 'ephemeral',
-  //     text: error
-  //   });
+    await respond({
+      response_type: 'ephemeral',
+      text: UNKNOWN_ERROR
+    });
+  }
+});
 
-  //   return;
-  // }
+app.command('/who_nested', async ({ command, ack, respond }) => {
+  await ack();
 
-  // try {
-  //   parkingStore.parked[parkSlot] = command.user_id;
+  try {
+    const allParked = await getAllParked();
+    let allParkedText;
+    if (!allParked.length) {
+      allParkedText = 'Рано! Ніхто сьогодні ще не запаркований'
+    } else {
+      allParkedText = allParked.map(parked => `${parked.parkingSlot}: <@${parked.userId}>`).join('\n')
+    }
 
-  //   await respond({
-  //     response_type: 'ephemeral',
-  //     text: `Parking slot ${parkSlot} is yours`
-  //   });
-  // } catch {
-  //   await respond({
-  //     response_type: 'ephemeral',
-  //     text: UNKNOWN_ERROR
-  //   });
-  // }
+    await app.client.chat.postEphemeral({
+      channel: command.channel_id,
+      user: command.user_id,
+      text: allParkedText
+    });
+  } catch (e) {
+    console.log('!!!Error', e);
+
+    await respond({
+      response_type: 'ephemeral',
+      text: UNKNOWN_ERROR
+    });
+  }
+});
+
+app.command('/nest', async ({ command, ack, respond }) => {
+  await ack();
+
+  try {
+    const parkSlot = +command.text;
+    let error;
+
+    if (typeof parkSlot !== 'number') {
+      error = `Я не розумію де ти хочеш запаркуватись. Треба передати номер місця після команди, наприклад "/park 121". А ти написав: ${command.text}.`
+    } else if (!availablePlaces.includes(parkSlot)) {
+      error = `Ти пробуєш запаркуватись на місце якого немає у дозволених. Доступні місця: ${availablePlaces.join(', ')}. А ти написав: ${command.text}.`;
+    } else {
+      const parked = await getIsSlotAlreadyBooked(parkSlot, command.user_id);
+      if (parked) {
+        if (parked.userId === command.user_id) {
+          error = `Ееее, за тобою вже заброньоване місце ${parked.parkingSlot}`
+        } else {
+          error = `На цьому місці вже приземлився <@${parked.userId}>`
+        }
+      }
+    }
+
+    if (error) {
+      await respond({
+        response_type: 'ephemeral',
+        text: error
+      });
+
+      return;
+    }
+
+    await addParkingStatement(command.user_id, parkSlot)
+
+    await respond({
+      response_type: 'ephemeral',
+      text: `Йой, успішно приземлився на місці ${parkSlot}`
+    });
+  } catch (e) {
+    console.log('!!!Error', e);
+
+    await respond({
+      response_type: 'ephemeral',
+      text: UNKNOWN_ERROR
+    });
+  }
+});
+
+app.command('/destroy_nest', async ({ command, ack, respond }) => {
+  await ack();
+
+  try {
+    await removeParkingStatement(command.user_id)
+
+    await respond({
+      response_type: 'ephemeral',
+      text: `Файно є, ще хтось може приземлитись`
+    });
+  } catch (e) {
+    console.log('!!!Error', e);
+
+    await respond({
+      response_type: 'ephemeral',
+      text: UNKNOWN_ERROR
+    });
+  }
+});
+
+app.command('/blame_nest', async ({ command, ack, respond }) => {
+  await ack();
+
+  try {
+    let [parkSlotText, carPlate] = command.text.split(' ');
+    const parkSlot = +parkSlotText;
+    const normalizedCarPlate = (carPlate || '').replace(/ /g, '').toUpperCase();
+
+    if (typeof parkSlot !== 'number') {
+      error = `Я не розумію яке паркомісце ти написав. Треба передати номер місця після команди, наприклад "/blame_nest 12 BC1212HE". А ти написав: ${command.text}.`
+    } else if (!availablePlaces.includes(parkSlot)) {
+      error = `Такого паркомісця немає у дозволених. Доступні місця: ${availablePlaces.join(', ')}. А ти написав: ${command.text}.`;
+    } else if (!normalizedCarPlate || normalizedCarPlate.length < 4 || normalizedCarPlate.length > 10) {
+      error = `Неправильно переданий номер машини. Треба передати номер місця і номер машини після команди, наприклад "/blame_nest 12 BC1212HE". А ти написав: ${command.text}.`;
+    } else {
+      const parked = await getIsSlotAlreadyBooked(parkSlot);
+      if (parked) {
+        error = `На цьому місці запаркований <@${parked.userId}>`
+      }
+    }
+
+    await addBlameStatement(normalizedCarPlate, parkSlot, command.user_id);
+
+    await respond({
+      response_type: 'ephemeral',
+      text: `Ухх, ми йому помстимося!`
+    });
+  } catch (e) {
+    console.log('!!!Error', e);
+
+    await respond({
+      response_type: 'ephemeral',
+      text: UNKNOWN_ERROR
+    });
+  }
+});
+
+app.command('/all_nest_blames', async ({ command, ack, respond }) => {
+  await ack();
+
+  try {
+    const blames = await getAllBlames();
+    let blamesText;
+    if (!blames.length) {
+      blamesText = 'Ого! Ніхто ще нікого не звинуватив'
+    } else {
+      blamesText = blames.map(blame => `${blame.carPlate}: ${blame.count}`).join('\n')
+    }
+
+    await app.client.chat.postEphemeral({
+      channel: command.channel_id,
+      user: command.user_id,
+      text: blamesText
+    });
+  } catch (e) {
+    console.log('!!!Error', e);
+
+    await respond({
+      response_type: 'ephemeral',
+      text: UNKNOWN_ERROR
+    });
+  }
 });
 
 (async () => {
-  // Start the app
   let port = process.env.PORT;
   if (port == null || port == "") {
     port = 8000;
   }
   await app.start(port);
 
-  const clientResult = await clientDemo();
-  console.log("DB connection works, Time with client: " + clientResult.rows[0]["now"]);
+  await setupDb();
 
-  console.log('⚡️ Bolt app is running!');
+  console.log('App is running!');
 })();
-
-
-
-    // await app.client.chat.postEphemeral({
-    //   channel: command.channel_id,
-    //   user: command.user_id,
-    //   text: `Ephemeral Message Test <@${command.user_id}>?`,
-    //   attachments: [
-    //     {
-    //       "type": "image",
-    //       "title": {
-    //         "type": "plain_text",
-    //         "text": "Parking schema"
-    //       },
-    //       "image_url": "https://slack-files.com/T437TQE4V-F03F39QFD3J-8e32c7691c",
-    //       "alt_text": "Parking schema"
-    //     }
-    //   ]
-    // });
